e01a5aac3fe6bba436a709bd4763fbd7d3e30ed6
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-03-24 20:11:51 +0530

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,16 +1,16 @@
-module.export = {
+module.exports = {
   name: 'ping',
-  description: "Replies with bot ping!"
+  description: "Replies with bot ping!",
   callback: async (client, interaction) => {
-  await interaction.deferReply();
+    await interaction.deferReply();
 
-  const reply = await interaction.fetchReply();
+    const reply = await interaction.fetchReply();
 
-  const ping = reply.createdTimestamp - interaction.createdtimestamp;
+    const ping = reply.createdTimestamp - interaction.createdTimestamp;
 
-  interaction.editReply(
-    `Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`   
-  );
- },
-};  
-  
+    interaction.editReply(
+      `Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`   
+    );
+  },
+};
+
